92a1d92ea6f68178688ea04f98d8d84c5ae576cb
NicolasIsepe <nicolasisepe@gmail.com>
2026-09-04 12:32:43 -0300

    refactor: Remove campo pais e altera o tipo dos campos estado e cep para char na tabela endereco

--- fisico_completo_fixa.sql
+++ fisico_completo_fixa.sql
@@ -78,9 +78,8 @@ CREATE TABLE endereco(
     complemento    VARCHAR(100),
     bairro         VARCHAR(100) NOT NULL,
     cidade         VARCHAR(100) NOT NULL,
-    estado         VARCHAR(2) NOT NULL,
-    pais           VARCHAR(100) NOT NULL,
-    cep            VARCHAR(10) NOT NULL,
+    estado         CHAR(2) NOT NULL,
+    cep            CHAR(8) NOT NULL,
     data_criacao   TIMESTAMP NOT NULL DEFAULT NOW(),
     esta_ativo     BOOLEAN NOT NULL DEFAULT TRUE
 );
