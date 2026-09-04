b5c33876d51cfc41518a8d919d69da62e4dc4ecb
NicolasIsepe <nicolasisepe@gmail.com>
2026-09-04 12:34:14 -0300

    refactor: Altera tipo do campo cnpj de varchar para char na tabela instituição

--- fisico_completo_fixa.sql
+++ fisico_completo_fixa.sql
@@ -56,13 +56,13 @@ CREATE TABLE super_admin(
 -- Dependências: -
 
 CREATE TABLE instituicao(
-    id                SERIAL PRIMARY KEY,
-    nome              VARCHAR(100) NOT NULL,
-    cnpj              VARCHAR(20) NOT NULL UNIQUE,
-    tipo_instituicao  INTEGER NOT NULL,
-    dominio_email     VARCHAR(100) NOT NULL UNIQUE,
-    data_criacao      TIMESTAMP NOT NULL DEFAULT NOW(),
-    esta_ativo        BOOLEAN NOT NULL DEFAULT TRUE
+    id               SERIAL PRIMARY KEY,
+    nome             VARCHAR(100) NOT NULL,
+    cnpj             CHAR(14) NOT NULL UNIQUE,
+    tipo_instituicao INTEGER NOT NULL,
+    dominio_email    VARCHAR(100) NOT NULL UNIQUE,
+    data_criacao     TIMESTAMP NOT NULL DEFAULT NOW(),
+    esta_ativo       BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 
--- tabelas/instituicao.sql
+++ tabelas/instituicao.sql
@@ -11,7 +11,7 @@
 CREATE TABLE instituicao(
     id               SERIAL PRIMARY KEY,
     nome             VARCHAR(100) NOT NULL,
-    cnpj             VARCHAR(20) NOT NULL UNIQUE,
+    cnpj             CHAR(14) NOT NULL UNIQUE,
     tipo_instituicao INTEGER NOT NULL,
     dominio_email    VARCHAR(100) NOT NULL UNIQUE,
     data_criacao     TIMESTAMP NOT NULL DEFAULT NOW(),
