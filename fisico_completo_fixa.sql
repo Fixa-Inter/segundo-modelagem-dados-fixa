b262839dc6e5c33986c94bc85837620110569853
NicolasIsepe <nicolasisepe@gmail.com>
2026-09-10 23:28:53 -0300

    refactor: Adiciona default current_date no campo data_inicio da tabela contrato

--- fisico_completo_fixa.sql
+++ fisico_completo_fixa.sql
@@ -112,7 +112,7 @@ CREATE TABLE contrato(
     id           SERIAL PRIMARY KEY,
     plano_id     INTEGER NOT NULL REFERENCES plano(id),
     endereco_id  INTEGER NOT NULL REFERENCES endereco(id),
-    data_inicio  DATE NOT NULL,
+    data_inicio  DATE NOT NULL DEFAULT CURRENT_DATE,
     data_fim     DATE NOT NULL,
     status       INTEGER NOT NULL DEFAULT 0, -- 0 = Ativo, 1 = Inativo, 2 = Cancelado
     data_criacao TIMESTAMP NOT NULL DEFAULT NOW()
