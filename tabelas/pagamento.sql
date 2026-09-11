a2e5348d16c87e253101f102052897964ff33e4e
NicolasIsepe <nicolasisepe@gmail.com>
2026-09-10 23:25:03 -0300

    refactor: Altera tipo do campo data_pagamento de date para timestamp na tabela de pagamento

--- tabelas/pagamento.sql
+++ tabelas/pagamento.sql
@@ -12,7 +12,7 @@
 CREATE TABLE pagamento(
     id               SERIAL PRIMARY KEY,
     contrato_id      INTEGER NOT NULL REFERENCES contrato(id),
-    data_pagamento   DATE NOT NULL,
+    data_pagamento   TIMESTAMP NOT NULL,
     valor_pago       DECIMAL(10,2) NOT NULL,
     status           INTEGER DEFAULT 0, -- 0 = Pendente, 1 = Aprovado, 2 = Rejeitado
     metodo_pagamento INTEGER NOT NULL,
