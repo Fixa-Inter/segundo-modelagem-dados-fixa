b5e192b278cff9acac9f1c7b5ee7d079ede11059
NicolasIsepe <nicolasisepe@gmail.com>
2026-09-13 21:42:01 -0300

    refactor: Adiciona data de último acesso em usuário, campo usado para controlar a inatividade

--- fisico_completo_fixa.sql
+++ fisico_completo_fixa.sql
@@ -150,18 +150,19 @@ CREATE TABLE pagamento(
 -- Dependências: usuario (auto-relacionamento)
 
 CREATE TABLE usuario (
-    id              SERIAL PRIMARY KEY,
-    gerente_id      INTEGER,
-    endereco_id     INTEGER NOT NULL REFERENCES endereco(id),
-    nome_completo   VARCHAR(100) NOT NULL,
-    email           VARCHAR(100) NOT NULL UNIQUE,
-    tipo_acesso     INTEGER NOT NULL,
-    senha_hash      VARCHAR(255) NOT NULL,
-    cargo           VARCHAR(100) NOT NULL,
-    data_nascimento DATE NOT NULL,
-    data_criacao    TIMESTAMP NOT NULL DEFAULT NOW(),
-    esta_ativo      BOOLEAN NOT NULL DEFAULT FALSE,
-    primeiro_acesso BOOLEAN NOT NULL DEFAULT TRUE,
+    id                 SERIAL PRIMARY KEY,
+    gerente_id         INTEGER,
+    endereco_id        INTEGER NOT NULL REFERENCES endereco(id),
+    nome_completo      VARCHAR(100) NOT NULL,
+    email              VARCHAR(100) NOT NULL UNIQUE,
+    tipo_acesso        INTEGER NOT NULL,
+    senha_hash         VARCHAR(255) NOT NULL,
+    cargo              VARCHAR(100) NOT NULL,
+    data_nascimento    DATE NOT NULL,
+    data_ultimo_acesso TIMESTAMP,
+    data_criacao       TIMESTAMP NOT NULL DEFAULT NOW(),
+    esta_ativo         BOOLEAN NOT NULL DEFAULT FALSE,
+    primeiro_acesso    BOOLEAN NOT NULL DEFAULT TRUE,
 
     CONSTRAINT ck_usuario_nao_e_proprio_gerente
         CHECK (gerente_id <> id),
