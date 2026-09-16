88ba8a30f2e15e17870e3df98e3affd9ff34149b
NicolasIsepe <nicolasisepe@gmail.com>
2026-09-16 08:37:38 -0300

    refactor: Altera os arquivos unitários das tabelas

--- tabelas/observacao_conclusao.sql
+++ tabelas/observacao_conclusao.sql
@@ -11,7 +11,7 @@
 
 CREATE TABLE observacao_conclusao(
     id               SERIAL PRIMARY KEY,
-    ordem_servico_id INTEGER NOT NULL REFERENCES ordem_servico(id),
+    ordem_servico_id INTEGER NOT NULL REFERENCES ordem_servico(id) ON DELETE CASCADE,
     observacao       VARCHAR(255) NOT NULL,
     data_criacao     TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- tabelas/tarefa_status_historico.sql
+++ tabelas/tarefa_status_historico.sql
@@ -11,8 +11,8 @@
  */
 
 CREATE TABLE tarefa_status_historico(
-    id                      SERIAL PRIMARY KEY,
-    tarefa_id               INTEGER NOT NULL REFERENCES tarefa(id),
-    status_ordem_servico_id INTEGER NOT NULL REFERENCES status_ordem_servico(id),
-    data_atualizacao        TIMESTAMP NOT NULL DEFAULT NOW()
+    id                       SERIAL PRIMARY KEY,
+    tarefa_id                INTEGER NOT NULL REFERENCES tarefa(id) ON DELETE CASCADE,
+    status_ordem_servico_id  INTEGER NOT NULL REFERENCES status_ordem_servico(id),
+    data_atualizacao         TIMESTAMP NOT NULL DEFAULT NOW()
 );
